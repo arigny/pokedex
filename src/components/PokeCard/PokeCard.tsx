4452700aaeec3d8205ac55596ac3bf9a092192ef
alex rigney <arign020@gmail.com>
2023-01-30 23:43:01 -0500

    pad pokemon ids

--- src/components/PokeCard/PokeCard.tsx
+++ src/components/PokeCard/PokeCard.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 // import './PokeCard.css'
 
 interface Pokemon {
-    id: number;
+    id: number | string;
     name: string;
     image: string;
     firstType: string;
@@ -14,11 +14,22 @@ export const PokeCard = ({id, name, image, firstType, secondType}: Pokemon) => {
         return name.slice(0,1).toUpperCase() + name.slice(1)
     }
 
+    const padId = () => {
+        const idLength = id.toString().length;
+        if (idLength === 1) {
+            id = "00" + id.toString();
+        }
+        else if (idLength === 2) {
+            id = "0" + id.toString();
+        }
+        return id;
+    }
+
     return (
         <div>
             <div className="card">
             <img src={image} />
-            <p className="id">#{id}</p>
+            <p className="id">#{padId()}</p>
             <p className="name">{capitalizedName()}</p>
             <p className='type'> {firstType} {secondType} </p>
             </div>
