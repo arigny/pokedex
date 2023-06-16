00d5c14aa58e9d9dc52f5953fa6e9594ba5c0bd5
alex rigney <arign020@gmail.com>
2023-06-16 02:36:51 -0400

    import searchIcon svg

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,3 +1,4 @@
+import searchIcon from './search.svg'
 import './Searchbar.css';
 
 export const Searchbar = ({search, setSearch}: any) => {
@@ -8,8 +9,8 @@ export const Searchbar = ({search, setSearch}: any) => {
 
     return (
         <div className="searchbar">
-            <input type='text' placeholder='What Pokemon are you looking for?' value={search} onChange={onSearchChange}></input>
-            <img className="search-icon" src="./assets/search.svg" alt="search-icon"></img>
+            <input type='text' placeholder='What Pokemon are you looking for?' value={search} onChange={onSearchChange} />
+            <img className="search-icon" src={searchIcon} alt="search-icon"></img>
         </div>
     )
 }
